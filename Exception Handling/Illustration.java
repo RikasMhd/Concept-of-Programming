35451c7c03eb6f13b11ee3d3114dbf70a403f076
Rikas <rrikas341@gmail.com>
2026-01-21 14:07:27 +0530

    Add files via upload

---
+++ Exception Handling/Illustration.java
@@ -0,0 +1,11 @@
+class Illustration{
+
+    public static void main(String args[])
+    {
+        // Taking an empty string
+        String s = null;
+      
+        // Getting length of a string
+        System.out.println(s.length());
+    }
+}
---
+++ Exception Handling/MultipleException.java
@@ -0,0 +1,16 @@
+try {
+    
+    // Code that may throw an exception
+    
+} catch (ArithmeticException e) {
+    
+    // Code to handle the exception
+    
+} catch(ArrayIndexOutOfBoundsException e){
+    
+    // Code to handle the another exception
+    
+}catch(NumberFormatException e){
+    
+     // Code to handle the another exception
+}
